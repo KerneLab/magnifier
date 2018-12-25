934ca8223c462865df1f13018e0a96001ef78dff
核心力量 <kernelforce@gmail.com>
2018-12-26 00:55:02 +0800

    EXEC_ID is available in test results.

--- CREATE_TABLE_MGNF_TEST_RECORDS.sql
+++ CREATE_TABLE_MGNF_TEST_RECORDS.sql
@@ -5,6 +5,6 @@ create table MGNF_TEST_RECORDS
   program  VARCHAR2(100) not null,
   line_id  INTEGER not null,
   sign_id  NUMBER not null,
-  logic_id INTEGER not null,
-  test_tm  DATE not null
+  exec_id  VARCHAR2(23) not null,
+  logic_id INTEGER not null
 );
